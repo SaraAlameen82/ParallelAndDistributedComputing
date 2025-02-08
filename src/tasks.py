490270026f6f4cd1ff96f67a29e14d81f5968e85
SaraAlameen82 <60101453@udst.edu.qa>
2025-02-08 21:58:56 +0300

    Update tasks.py

--- src/tasks.py
+++ src/tasks.py
@@ -3,13 +3,11 @@ import string
 
 # Function to join a thousand random letters
 def join_random_letters(num_letters):
-  letters = [random.choice(string.ascii_letters) for _ in range(1000)] 
-  joined_letters = ''.join(letters)
+  letters = ''.join(random.choice(string.ascii_letters) for _ in range(num_letters)) 
   print("Joined Letters Task Done")
 
 
 # Function to add a thousand random numbers
 def add_random_numbers(num_numbers):
-  numbers = [random.randint(1, 100) for _ in range(1000)]
-  total_sum = sum(numbers)
+  numbers = sum(random.randint(1, 100) for _ in range(num_numbers))
   print("Add Numbers Task Done")
