31050a7698f0c3d95f4c8a72ff9034fada9794e1
Sara Mohamed <60101453@udst.edu.qa>
2025-02-09 11:39:27 +0300

    saving lab 2

---
+++ src/.ipynb_checkpoints/tasks-checkpoint.py
@@ -0,0 +1,18 @@
+import threading
+import time
+import random
+import string
+
+
+# Function to join a thousand random letters
+def join_random_letters():
+	letters = [random.choice(string.ascii_letters) for _ in range(1000)] 
+    joined_letters = ''.join(letters) 
+    print("Joined Letters Task Done")
+
+
+# Function to add a thousand random numbers
+def add_random_numbers():
+    numbers = [random.randint(1, 100) for _ in range(1000)] 
+    total_sum = sum(numbers) 
+    print("Add Numbers Task Done")
