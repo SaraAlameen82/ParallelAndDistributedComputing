35cc1497fe978beb1bff3421f68162d1b7c99ce4
SaraAlameen82 <60101453@udst.edu.qa>
2025-02-15 19:03:06 +0300

    Update threads_test.py

--- src/threads_test.py
+++ src/threads_test.py
@@ -1,4 +1,5 @@
-from src.tasks import *
+from src.tasks import join_random_letters 
+from src.tasks import add_random_numbers 
 import threading
 import time
 import random
@@ -6,16 +7,16 @@ import string
 
 
 def run_threads(num_letters = 1000, num_numbers = 1000):
-    num_letters = 1000 // 2
-    num_numbers = 1000 // 2
+    num_letters = num_letters // 2
+    num_numbers = num_numbers // 2
     start_time = time.time()
     
     # Create two processes
-    thread_numbers1 = threading.Thread(target=add_random_numbers, args=(num_numbers))
-    thread_numbers2 = threading.Thread(target=add_random_numbers, args=(num_numbers))
+    thread_numbers1 = threading.Thread(target=add_random_numbers, args=(num_numbers,))
+    thread_numbers2 = threading.Thread(target=add_random_numbers, args=(num_numbers,))
     
-    thread_letters1 = threading.Thread(target=join_random_letters, args=(num_letters))
-    thread_letters2 = threading.Thread(target=join_random_letters, args=(num_letters))
+    thread_letters1 = threading.Thread(target=join_random_letters, args=(num_letters,))
+    thread_letters2 = threading.Thread(target=join_random_letters, args=(num_letters,))
 
     # Start the processes
     thread_numbers1.start()
@@ -33,6 +34,6 @@ def run_threads(num_letters = 1000, num_numbers = 1000):
 
     end_time = time.time()
     excution_time = end_time - start_time
-    print(f"Total time for threads: {excution_time} seconds.")
+    # print(f"Total time for threads: {excution_time} seconds.")
     
     return excution_time
