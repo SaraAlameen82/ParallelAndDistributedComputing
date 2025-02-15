cfd7d713dc3ebabdfa79af743a899c771982d4eb
SaraAlameen82 <60101453@udst.edu.qa>
2025-02-15 14:55:15 +0300

    Update threads_test.py

--- src/threads_test.py
+++ src/threads_test.py
@@ -11,25 +11,25 @@ def run_threads(num_letters = 1000, num_numbers = 1000):
     start_time = time.time()
     
     # Create two processes
-    process_numbers1 = threading.thread(target=add_random_numbers, args=(num_numbers))
-    process_numbers2 = threading.thread(target=add_random_numbers, args=(num_numbers))
+    thread_numbers1 = threading.Thread(target=add_random_numbers, args=(num_numbers))
+    thread_numbers2 = threading.Thread(target=add_random_numbers, args=(num_numbers))
     
-    process_letters1 = threading.thread(target=join_random_letters, args=(num_letters))
-    process_letters2 = threading.thread(target=join_random_letters, args=(num_letters))
+    thread_letters1 = threading.Thread(target=join_random_letters, args=(num_letters))
+    thread_letters2 = threading.Thread(target=join_random_letters, args=(num_letters))
 
     # Start the processes
-    process_numbers1.start()
-    process_numbers2.start()
+    thread_numbers1.start()
+    thread_numbers2.start()
 
-    process_letters1.start()
-    process_letters2.start()
+    thread_letters1.start()
+    thread_letters2.start()
 
     # Wait for all processes to complete
-    process_numbers1.join()
-    process_numbers2.join()
+    thread_numbers1.join()
+    thread_numbers2.join()
 
-    process_letters1.join()
-    process_letters2.join()
+    thread_letters1.join()
+    thread_letters2.join()
 
     end_time = time.time()
     excution_time = end_time - start_time
