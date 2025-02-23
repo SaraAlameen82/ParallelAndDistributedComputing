92bf4025a912365fee5107aeafd16857c1abd9a4
Sara Mohamed <60101453@udst.edu.qa>
2025-02-23 13:15:26 +0300

    Starting lab 4 part 1.

---
+++ .ipynb_checkpoints/sensor_simulation-checkpoint.py
@@ -0,0 +1,14 @@
+import random
+import time
+import threading
+from queue import Queue
+
+# Global dictionaries
+latest_temperatures = {}
+temperature_averages = {}
+
+
+def simulate_sensor(latest_temperatures, ):
+    temp = random.randint(15, 40)
+
+
